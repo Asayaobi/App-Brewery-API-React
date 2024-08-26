e18cf3329b98871173dca736c1ec0e11f1a9000b
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-08-26 18:55:15 +0200

    add get /filter route with req.body in index.js

--- API/6.0 DIY API/index.js
+++ API/6.0 DIY API/index.js
@@ -22,7 +22,11 @@ app.get("/jokes/:id", (req,res) =>{
 })
 
 //3. GET a jokes by filtering on the joke type
-
+app.get("/filter", (req,res) =>{
+    const type = req.body.type
+    const findJokes = jokes.filter((joke) => joke.jokeType === type)
+    res.json(findJokes)
+})
 //4. POST a new joke
 
 //5. PUT a joke
