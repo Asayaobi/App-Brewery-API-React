77471f44c6d8d2348ea2947133160c062a19b004
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-08-26 17:04:47 +0200

    Create an express app and set the port number.

--- API/5.6 Secrets Project/index.js
+++ API/5.6 Secrets Project/index.js
@@ -1,8 +1,16 @@
 // HINTS:
 // 1. Import express and axios
+import express from 'express'
+import axios from 'axios'
 
 // 2. Create an express app and set the port number.
 
+const app = express()
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`server is running on ${port}`)
+})
 // 3. Use the public folder for static files.
 
 // 4. When the user goes to the home page it should render the index.ejs file.
