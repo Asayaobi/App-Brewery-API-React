7f9536cd8b39d06a18820e626becca35f4760ec8
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-08-26 17:20:46 +0200

    Use axios to get a random secret and pass it to index.ejs

--- API/5.6 Secrets Project/index.js
+++ API/5.6 Secrets Project/index.js
@@ -12,12 +12,16 @@ const port = 3000
 app.use(express.static('public'))
 
 // 4. When the user goes to the home page it should render the index.ejs file.
-app.get("/", (req,res) => {
-    res.render('index.ejs')
-})
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
-
+app.get("/", async (req,res) => {
+    try{
+        const response = await axios.get('https://secrets-api.appbrewery.com/random')
+    res.render('index.ejs', {content: response.data})
+    } catch (error){
+        console.error("error:", error.message)
+    }
+})
 // 6. Listen on your predefined port and start the server.
 app.listen(port, () => {
     console.log(`server is running on ${port}`)
