28341e1e5c49dfc0c4b09b6236eed28940c22a96
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-08-31 20:40:47 +0200

    import pg and connection

--- Postgres/8.3 Travel Tracker/index.js
+++ Postgres/8.3 Travel Tracker/index.js
@@ -1,6 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg"
 
+//create a connection
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "World",
+  password: "AsayaObi",
+  port: 5432,
+})
+db.connect()
 const app = express();
 const port = 3000;
 
