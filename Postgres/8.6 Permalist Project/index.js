77e6ec68bcc2aedc4b626393dcc824d59a7ac5b0
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-02 20:23:27 +0200

    create database connection with pg

--- Postgres/8.6 Permalist Project/index.js
+++ Postgres/8.6 Permalist Project/index.js
@@ -1,11 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from 'pg'
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "Permalist",
+  password: "AsayaObi",
+  port: 5432,
+})
+
+db.connect()
 
 let items = [
   { id: 1, title: "Buy milk" },
