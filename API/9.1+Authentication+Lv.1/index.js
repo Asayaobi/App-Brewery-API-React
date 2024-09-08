b43ae66cbe20046ec02cee8e3ca3bcc7cd28f175
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-08 17:23:13 +0200

    add post/register index.js

--- API/9.1+Authentication+Lv.1/index.js
+++ API/9.1+Authentication+Lv.1/index.js
@@ -32,7 +32,24 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const email = req.body.username
+  const password = req.body.password
+  try{
+  //first check if the email already exist
+  let result = await db.query('SELECT * FROM users WHERE email = $1', [email])
+  //if the email exists, send back the error
+  if (result.rows.length > 0){
+    res.send('Email already exists, try logging in')
+  } else {
+  //if email doesn't exist, create new user
+    await db.query('INSERT INTO users(email, password) VALUES ($1, $2)', [email,password])
+    res.render('secrets.ejs')
+  }
+  } catch (err){
+    console.log(err)
+  }
+})
 
 app.post("/login", async (req, res) => {});
 
