233e781af91576917c116ee4b1061d254c93bd1e
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-12 18:31:37 +0200

    import google auth

--- API/9.5+Authentication+Lv.3/index.js
+++ API/9.5+Authentication+Lv.3/index.js
@@ -6,6 +6,7 @@ import passport from "passport";
 import { Strategy } from "passport-local";
 import session from "express-session";
 import env from "dotenv";
+import GoogleStrategy from "passport-google-oauth2"
 
 const app = express();
 const port = 3000;
@@ -138,6 +139,16 @@ passport.use(
     }
   })
 );
+//impliment GoogleStrategy from "passport-google-oauth2" under passport.use
+passport.use("google", new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+}, async (accessToken, refreshToken, profile, cb) => {
+  //what google profile contains
+  console.log(profile)
+}))
 
 passport.serializeUser((user, cb) => {
   cb(null, user);
