7743f59cfc088ae7c892596f166886d59219298b
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-15 21:05:50 +0200

    add image attribute

--- React/280-jsx-code-challenge/src/index.jsx
+++ React/280-jsx-code-challenge/src/index.jsx
@@ -13,14 +13,17 @@ import ReactDOM from "react-dom"
 
 //variable
 const year = new Date().getFullYear()  // returns the current year
-
+const randomPic = "https://picsum.photos/200"
 ReactDOM.render(
     <div>
-        <h1>My Favorite Food</h1>
+        <h1 className="heading">My Favorite Food</h1>
         <ul>
+            <img className="food-img" alt="Jamon" src="https://spainiberico.com/wp-content/uploads/nc/products/Jamon_bellota_100_iberico_green_label.jpg"/>
             <li>Jamon Iberico</li>
+            <img className="food-img" alt="BBQ" src="https://cloudfront-us-east-1.images.arcpublishing.com/pmn/WQS5BGVCHZATLDQOQ6XOKTGLAM.jpg"/>
             <li>Korean BBQ</li>
-            <li>Papaya Salad</li>
+            <img className="food-img" alt="random" src={randomPic + "?grayscale"}/>
+            <li>Surprise me!</li>
         </ul>
         <p> Created by Asaya Obi</p>
         <p>Copyright {year}</p>
