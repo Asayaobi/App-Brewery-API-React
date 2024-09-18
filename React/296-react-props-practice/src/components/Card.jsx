fd6cd14375780c991545545dbd7aa2533ccab420
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-18 15:12:42 +0200

    passing img props to Avartar.jsx

--- React/296-react-props-practice/src/components/Card.jsx
+++ React/296-react-props-practice/src/components/Card.jsx
@@ -1,16 +1,15 @@
 import React from "react"
+import Avatar from "./Avatar"
 
 function Card(props) {
+    //receiving props of contacts from App.jsx
     console.log(props)
+    //passing img props to Avartar.jsx
     return (
         <div className="card">
           <div className="top">
             <h2 className="name">{props.name}</h2>
-            <img
-              src={props.img}
-              alt="avatar_img"
-              className="circle-img"
-            />
+            <Avatar img={props.img} />
           </div>
           <div className="bottom">
             <p className="info">{props.phone}</p>
