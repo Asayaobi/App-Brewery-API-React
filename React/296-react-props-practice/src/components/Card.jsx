6410d2a5b6a17904cd158efedce08756eeb820f0
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-19 17:05:10 +0200

    Update Card.jsx

--- React/296-react-props-practice/src/components/Card.jsx
+++ React/296-react-props-practice/src/components/Card.jsx
@@ -1,20 +1,19 @@
 import React from "react"
 import Avatar from "./Avatar"
+import Detail from "./Detail"
 
 function Card(props) {
-    //receiving props of contacts from App.jsx
+    //receiving props of all deatils of 'contacts' from App.jsx
     console.log(props)
     //passing img props to Avartar.jsx
+    //passing phone and email as props to Detail.jsx
     return (
         <div className="card">
           <div className="top">
             <h2 className="name">{props.name}</h2>
             <Avatar img={props.img} />
           </div>
-          <div className="bottom">
-            <p className="info">{props.phone}</p>
-            <p className="info">{props.email}</p>
-          </div>
+            <Detail phone={props.phone} email={props.email}/>
         </div>
       )
 }
