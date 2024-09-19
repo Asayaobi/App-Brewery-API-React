2cb8b2caffd65b5e37a3dcccc8adfcba16411c23
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-19 17:22:49 +0200

    Update App.jsx

--- React/296-react-props-practice/src/components/App.jsx
+++ React/296-react-props-practice/src/components/App.jsx
@@ -2,17 +2,23 @@ import React from "react"
 import Card from "./Card"
 import contacts from "../contacts"
 
+// mapping contacts array
+function createCard(contact) {
+  return (<Card 
+    key={contact.id}
+    id={contact.id}
+    name={contact.name} 
+    img={contact.imgURL} 
+    phone={contact.phone} 
+    email={contact.email}/>)
+}
+
 function App() {
   //passing contacts array with props to Card.jsx
   return (
     <div>
         <h1 className="heading">My Contacts</h1>
-        {contacts.map(contact => (
-          <Card name={contact.name} 
-          img={contact.imgURL} 
-          phone={contact.phone} 
-          email={contact.email}/>
-        ))}
+        {contacts.map(createCard)}
     </div>
   )
 }
