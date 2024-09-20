1e673172b4da00454849a57dddf8e9572161fccc
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-20 18:44:04 +0200

    Create App.jsx

---
+++ React/303-conditional-rendering/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import Login from "./LogIn"
+
+var isLoggedin = false
+//if the user logged in -> show <h1>Hello</h1>
+//if not loggeg in -> show form to input username/ password
+
+let currentTime = new Date().getHours()
+currentTime = 11
+function App() {
+  return (
+    <div className="container">
+      {/* {isLoggedin ? <h1>Hello</h1> : <Login />} */}
+      {currentTime < 12 && <h1>Why are you still working?</h1>}
+    </div>
+  )
+}
+
+export default App
