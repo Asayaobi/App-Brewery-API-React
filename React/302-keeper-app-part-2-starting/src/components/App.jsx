0ef795f9334e5e962a28547c9998c0145b51f9e6
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-20 17:43:15 +0200

    Update App.jsx

--- React/302-keeper-app-part-2-starting/src/components/App.jsx
+++ React/302-keeper-app-part-2-starting/src/components/App.jsx
@@ -1,16 +1,32 @@
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
+import React from "react"
+import Header from "./Header"
+import Footer from "./Footer"
+import Note from "./Note"
+import notes from "../notes"
+
+
+//const notes = [
+// {
+//     key: 1,
+//     title: "Delegation",
+//     content:
+//       "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
+//   }]
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+
+      {notes.map(note => (
+        <Note key={note.key}
+        title={note.title}
+        content = {note.content}/>
+      ))}
+      
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
