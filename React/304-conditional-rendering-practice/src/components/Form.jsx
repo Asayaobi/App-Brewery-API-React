ce150fee424e634dfecb62ca20ab40c860a084ad
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-20 21:14:54 +0200

    Update Form.jsx

--- React/304-conditional-rendering-practice/src/components/Form.jsx
+++ React/304-conditional-rendering-practice/src/components/Form.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React from "react"
 
-function Form() {
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
+
+function Form(props) {
+  var userIsRegistered = props.userIsRegistered
   return (
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+       {!userIsRegistered && <input type="password" placeholder="Confirm Password" />}
+      <button type="submit">
+        {userIsRegistered ? "Log In" : "Register"}
+      </button>
     </form>
-  );
+  )
 }
 
-export default Form;
+export default Form
