77a36897c6ccbcf488efb3d68a41f6fc8613ec69
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-21 17:52:48 +0200

    Update App.jsx

--- React/307-useState-hook-practice/src/components/App.jsx
+++ React/307-useState-hook-practice/src/components/App.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useState} from "react"
+
 
 function App() {
-  return (
+//Show the latest time when the Get Time button is pressed
+  let now = new Date().toLocaleTimeString()
+  const [time, setTime] = useState(now)
+
+  function updateTime(){
+    now = new Date().toLocaleTimeString()
+    setTime(now)
+  }
+
+//using the setInterval method to update the time automatically every second
+  setInterval(updateTime, 1000)
+
+    return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
