174a74a24571d6e8185820d2ac66628d9b051b5b
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-22 21:26:46 +0200

    add spread operator

--- React/314-changing-complex-state-practice/src/components/App.jsx
+++ React/314-changing-complex-state-practice/src/components/App.jsx
@@ -1,22 +1,38 @@
 import React, { useState } from "react";
 
 function App() {
-  const [contact, setContact] = useState({
+  const [inputContact, setInputContact] = useState({
     fName: "",
     lName: "",
     email: ""
-  });
+  })
+  const [contact, setContact] = useState({})
 
+  function handleChange(event) {
+    const {name, value} = event.target    
+    
+    //[name] is the ES6 syntax to get js variable event.target.name
+    setInputContact(prevContact => {
+      return {
+        ...prevContact, 
+        [name]: value }
+    }
+  )}
+
+    function handleClick(event) {
+      setContact(inputContact)
+      event.preventDefault()
+    }
   return (
     <div className="container">
       <h1>
         Hello {contact.fName} {contact.lName}
       </h1>
       <p>{contact.email}</p>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+      <form onSubmit={handleClick}>
+        <input onChange={handleChange} name="fName" placeholder="First Name" value={inputContact.fName}/>
+        <input onChange={handleChange} name="lName" placeholder="Last Name" value={inputContact.lName}/>
+        <input onChange={handleChange} name="email" placeholder="Email" value={inputContact.email}/>
         <button>Submit</button>
       </form>
     </div>
