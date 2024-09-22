cdf96f112adb46fdd1cbcb996f83c7fdf7456789
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-22 20:16:28 +0200

    Add useState object

--- React/313-changing-complex-state/src/components/App.jsx
+++ React/313-changing-complex-state/src/components/App.jsx
@@ -1,23 +1,37 @@
 import React, { useState } from "react";
 
 function App() {
-  const [fName, setfName] = useState("")
-  const [lName, setlName] = useState("")
+  const [fullName, setfullName] = useState({
+    fName: "",
+    lName: ""
+  })
 
-  function handlefName(event){
-    const firstName = event.target.value
-    setfName(firstName)
+  function handleChange(event){
+    const value = event.target.value //whatever you are typing Y a r o..
+    const input = event.target.name //the name of the input that you're typing fName
+
+    //when passing object, without prevValue, it'll reset the currect value and lose the prev value eg. get firstname and lastname blank
+    setfullName(prevValue => {
+      if (input === 'fName'){
+        return {
+          fName: value,
+          lName: prevValue.lName
+        }
+      } else if (input === 'lName') {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        }
+      }
+    })
   }
-  function handlelName(event){
-    const lastName = event.target.value
-    setlName(lastName)
-  }
+
   return (
     <div className="container">
-      <h1>Hello {fName} {lName}</h1>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
       <form>
-        <input onChange={handlefName} name="fName" placeholder="First Name" value={fName}/>
-        <input onChange={handlelName} name="lName" placeholder="Last Name" value={lName}/>
+        <input onChange={handleChange} name="fName" placeholder="First Name" value={fullName.fName}/>
+        <input onChange={handleChange} name="lName" placeholder="Last Name" value={fullName.lName}/>
         <button>Submit</button>
       </form>
     </div>
