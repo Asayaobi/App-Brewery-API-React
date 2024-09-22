fa08388aa5965a3c7abe55c592b8a2b520260fac
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-22 18:18:16 +0200

    Add useState hooks

--- React/313-changing-complex-state/src/components/App.jsx
+++ React/313-changing-complex-state/src/components/App.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fName, setfName] = useState("")
+  const [lName, setlName] = useState("")
+
+  function handlefName(event){
+    const firstName = event.target.value
+    setfName(firstName)
+  }
+  function handlelName(event){
+    const lastName = event.target.value
+    setlName(lastName)
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fName} {lName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input onChange={handlefName} name="fName" placeholder="First Name" value={fName}/>
+        <input onChange={handlelName} name="lName" placeholder="Last Name" value={lName}/>
         <button>Submit</button>
       </form>
     </div>
