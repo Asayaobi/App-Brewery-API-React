e7482afd672a2cd30cf75d62a2434f2cdeffdc78
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-23 18:24:16 +0200

    add onChange event

--- React/316-es6-spread-operator-practice/src/components/App.jsx
+++ React/316-es6-spread-operator-practice/src/components/App.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  //1. When new text is written into the input, its state should be saved.
+  const [inputText, setInputText] = useState("")
+  //2. When the add button is pressed, the current data in the input should be
+//added to an array.
+  function handleChange(event) {
+    const value = event.target.value
+    setInputText(value)
+  }
   return (
     <div className="container">
       <div className="heading">
@@ -9,6 +17,7 @@ function App() {
       <div className="form">
         <input type="text" />
         <button>
+        <input onChange={handleChange} type="text" value={inputText}/>
           <span>Add</span>
         </button>
       </div>
