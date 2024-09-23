b02c2f850a55c430a517c7892dcc0a8251a4674e
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-23 18:24:37 +0200

    add onClick event

--- React/316-es6-spread-operator-practice/src/components/App.jsx
+++ React/316-es6-spread-operator-practice/src/components/App.jsx
@@ -3,11 +3,21 @@ import React, { useState } from "react";
 function App() {
   //1. When new text is written into the input, its state should be saved.
   const [inputText, setInputText] = useState("")
+  const[lists, setLists] = useState([])
+
   //2. When the add button is pressed, the current data in the input should be
 //added to an array.
   function handleChange(event) {
     const value = event.target.value
     setInputText(value)
+  }
+  
+  function handleClick() {
+    setLists(prevLists => {
+      return [...prevLists, inputText]
+      })
+    //clear the inputText for input UI
+    setInputText("")
   }
   return (
     <div className="container">
@@ -15,9 +25,8 @@ function App() {
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
         <input onChange={handleChange} type="text" value={inputText}/>
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
