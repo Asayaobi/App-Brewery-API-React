60dbc5095d67181083f09fd1c00dfd17ded32b0e
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-09-26 21:12:25 +0200

    Add animation to CreaeArea.jsx

--- React/320-styling-the-keeper-app-starting/src/components/CreateArea.jsx
+++ React/320-styling-the-keeper-app-starting/src/components/CreateArea.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
+import AddCircle from "@mui/icons-material/AddCircle"
+import Fab from '@mui/material/Fab'
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
-  });
+  })
+  //set the in props for zoom animation
+  const [isZoomed, setIsZoomed] = useState(false)
 
+  //when the text area is clicked isZoomed is true
+  function expand() {
+    setIsZoomed(true)
+  }
   function handleChange(event) {
     const { name, value } = event.target;
-
     setNote(prevNote => {
       return {
         ...prevNote,
@@ -26,23 +34,32 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+
   return (
     <div>
       <form className="create-note">
+        {isZoomed &&
         <input
           name="title"
           onChange={handleChange}
           value={note.title}
           placeholder="Title"
         />
+        }
         <textarea
           name="content"
           onChange={handleChange}
+          onClick={expand}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={isZoomed ? "3" : "1"}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={isZoomed}>
+          <Fab onClick={submitNote}>
+          <AddCircle />
+        </Fab>
+        </Zoom>
+
       </form>
     </div>
   );
