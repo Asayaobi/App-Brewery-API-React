bb9d021dd4ea2a6ec1440ac4dedca23519b863d7
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-11-05 17:54:04 +0100

    Update index.js

--- API/Personal Project/index.js
+++ API/Personal Project/index.js
@@ -1,53 +1,57 @@
-import express from "express";
-import bodyParser from "body-parser";
-import quotes from './quotes.json' assert { type: 'json' };
+import express from "express"
+import bodyParser from "body-parser"
+import data from './quotes.json' assert { type: 'json' }
 
-//quotes from quotes.json
+//data from quotes.json
+let quotes = data
 console.log('quotes', quotes)
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 //without assert { type: 'json' } -> you need to convert json to javascript object with JSON.parse
 // const quotes = JSON.parse(quotesJSON)
 // console.log('quotes',quotes)
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
 
+//render quotes in index.ejs
 app.get("/", (req, res) => {
-  res.render("index.ejs",{quotes:quotes});
-});
+  res.render("index.ejs",{quotes:quotes})
+})
 
-// guide
-// app.post("/recipe", (req, res) => {
-//   if (req.body.choice === 'chicken') {
-//     data = recipeObj[0]
-//     res.render("index.ejs",{taco:data})
-//   } else if (req.body.choice === 'beef') {
-//     data = recipeObj[1]
-//     res.render("index.ejs",{taco:data})
-//   } else if (req.body.choice === 'fish') {
-//     data = recipeObj[2]
-//     res.render("index.ejs",{taco:data})
-//   }
+// add new post
+app.post("/addquote", (req, res) => {
+  const {name, profilepicture, quote} = req.body
+  let newquote
 
-  // app.post("/recipe", (req, res) => {
-  //   switch (req.body.choice) {
-  //     case "chicken":
-  //     data = recipeObj[0]
-  //     break;
-  //     case 'beef': 
-  //     data = recipeObj[1]
-  //     break;
-  //     case 'fish': 
-  //     data = recipeObj[2]
-  //     break;
-  //   }
-  //   res.render("index.ejs",{taco:data})
-  //   })
+  //random profile pictures
+  const pictures = ["https://openmoji.org/data/color/svg/1F49C.svg", "https://openmoji.org/data/color/svg/1F335.svg", "https://openmoji.org/data/color/svg/1F421.svg", "https://openmoji.org/data/color/svg/1F438.svg","https://openmoji.org/data/color/svg/1F9A5.svg"]
+  const random = Math.floor(Math.random() * pictures.length)
 
+  if (req.body.profilepicture){
+      newquote = {
+      id: quotes.length + 1,
+      name: req.body.name,
+      profilepicture: req.body.profilepicture,
+      quote: req.body.quote
+    }
+  } else {
+    newquote = {
+      id: quotes.length + 1,
+      name: req.body.name,
+      profilepicture: pictures[random],
+      quote: req.body.quote
+    }
+  }
+
+  quotes.push(newquote)
+  console.log(quotes)
+
+  res.render("index.ejs",{quotes:quotes})
+})
 
 app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
+  console.log(`Server running on port: ${port}`)
+})
