2567e44cf03c14e73f274c919366c3d207021030
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-11-07 13:12:07 +0100

    Create App.jsx

---
+++ React/Personal-Project-Quirky-Quote/my-react-app/src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Header from "./Header"
+import Hero from "./Hero"
+import Features from "./Features"
+import Post from "./Post"
+import Quotes from "./Quotes"
+import Footer from "./Footer"
+
+function App() {
+  return (
+    <div>
+        <Header />
+        <Hero />
+        <Features />
+        <Post />
+        <Quotes />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
