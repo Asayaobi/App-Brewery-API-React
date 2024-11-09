e309b9cd785e6c0c030bd889fbb944708dbcf8b2
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-11-09 12:34:36 +0100

    Passing and receiving props from App.jsx to Post.jsx

--- React/Personal-Project-Quirky-Quote/my-react-app/src/components/App.jsx
+++ React/Personal-Project-Quirky-Quote/my-react-app/src/components/App.jsx
@@ -4,14 +4,25 @@ import Hero from "./Hero"
 import Features from "./Features"
 import Post from "./Post"
 import Footer from "./Footer"
+import quotes from "../quotes"
+
 
 function App() {
+  let data = quotes
+  console.log('data',data)
+
+  function postQuote(updateQuotes){
+    console.log('quotes update from post', updateQuotes)
+    data = updateQuotes
+    console.log('new data', data)
+  }
+
   return (
     <div>
         <Header />
         <Hero />
         <Features />
-        <Post />
+        <Post quotes={data} updateData={postQuote}/>
         <Footer />
     </div>
   )
