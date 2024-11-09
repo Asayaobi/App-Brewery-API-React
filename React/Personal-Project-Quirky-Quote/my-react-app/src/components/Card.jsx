e393215abd64cfab2c8d274e6daa100853957645
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-11-09 18:39:03 +0100

    update styles in card component

--- React/Personal-Project-Quirky-Quote/my-react-app/src/components/Card.jsx
+++ React/Personal-Project-Quirky-Quote/my-react-app/src/components/Card.jsx
@@ -4,12 +4,20 @@ function Card(props) {
     // console.log('props',props)
     return (
         <div className="col">
-          <div className="card mb-4 rounded-3 shadow-sm py-5 px-3 px-lg-5 border-white">
-            <div className="fs-3 py-3">
+          <div className="card mb-4 rounded-3 shadow-sm border-white">
+          
+          <div className="d-flex justify-content-end">
+            <button className="icon-link icon-link-hover p-0 border-0 bg-transparent">
+                <img src="https://openmoji.org/data/color/svg/1F49F.svg" height="50" width="50"/>
+                <div className="visually-hidden">{props.id}</div>
+              </button>
+            </div>
+
+            <div className="fs-3 py-3 px-3 px-lg-5">
               {props.text}
             </div>
 
-            <div className="py-5 row row-cols-1 row-cols-md-2 d-flex align-items-center">
+            <div className="py-5 row row-cols-1 row-cols-lg-2 d-flex align-items-center px-3 px-lg-5">
               <div className="col-lg-5">
                 <img
                   src={props.img}
