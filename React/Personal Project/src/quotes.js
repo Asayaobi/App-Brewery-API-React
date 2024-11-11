4c9a6945fd18106cf7c21189e0792340d64bc0ae
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-11-11 12:43:05 +0100

    Update quotes.js

--- React/Personal Project/src/quotes.js
+++ React/Personal Project/src/quotes.js
@@ -7,19 +7,19 @@ const quotes = [
     },
     {
       id: 2,
-      name: "Amanda E.",
+      name: "Amanda E",
       profilepicture: "https://i.postimg.cc/kMsMQh0z/randomuser1.png",
       quote: "Every day is a fresh start. But just to be clear, I’m going back to bed after this one."
     },
     {
       id: 3,
-      name: "Jessi J.",
+      name: "Jessi J",
       profilepicture: "https://i.postimg.cc/ZnMXyLBs/Screenshot-2567-11-05-at-15-51-43.png",
       quote: "Older people shouldn't eat healthy food, they need all the preservatives they can get."
     },
     {
       id: 4,
-      name: "Emily C.",
+      name: "Emily C",
       profilepicture: "https://i.postimg.cc/QtbDqHy1/randomuser2.png",
       quote: "The first 5 days after the weekend are always the hardest."
     },
