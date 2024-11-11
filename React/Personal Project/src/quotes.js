fd900ce2ce857689bd5a8d67c236088352ee3b6f
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-11-11 20:37:35 +0100

    Update quotes.js

--- React/Personal Project/src/quotes.js
+++ React/Personal Project/src/quotes.js
@@ -3,31 +3,36 @@ const quotes = [
       id: 1,
       name: "Mr. Winston",
       profilepicture: "https://i.postimg.cc/Xv1KW1TP/Screenshot-2567-11-05-at-15-47-35.png",
-      quote: "I may be drunk, Miss, but in the morning I will be sober and you will still be ugly."
+      quote: "I may be drunk, Miss, but in the morning I will be sober and you will still be ugly.",
+      feature: false
     },
     {
       id: 2,
       name: "Amanda E",
       profilepicture: "https://i.postimg.cc/kMsMQh0z/randomuser1.png",
-      quote: "Every day is a fresh start. But just to be clear, I’m going back to bed after this one."
+      quote: "Every day is a fresh start. But just to be clear, I’m going back to bed after this one.",
+      feature: false
     },
     {
       id: 3,
       name: "Jessi J",
       profilepicture: "https://i.postimg.cc/ZnMXyLBs/Screenshot-2567-11-05-at-15-51-43.png",
-      quote: "Older people shouldn't eat healthy food, they need all the preservatives they can get."
+      quote: "Older people shouldn't eat healthy food, they need all the preservatives they can get.",
+      feature: false
     },
     {
       id: 4,
       name: "Emily C",
       profilepicture: "https://i.postimg.cc/QtbDqHy1/randomuser2.png",
-      quote: "The first 5 days after the weekend are always the hardest."
+      quote: "The first 5 days after the weekend are always the hardest.",
+      feature: false
     },
     {
       id: 5,
       name: "Dr. Pepper",
       profilepicture: "https://i.postimg.cc/vBPNpvz8/Screenshot-2567-11-05-at-15-53-12.png",
-      quote: "A man who correctly guesses a woman’s age may be smart, but he’s not very bright."
+      quote: "A man who correctly guesses a woman’s age may be smart, but he’s not very bright.",
+      feature: true
     }
   ]
   
