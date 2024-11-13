53fb11499fa3cc2068ade136a18b8946382ba864
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-11-13 12:25:20 +0100

    add responsive layout to nav bar

--- React/Personal Project/src/components/Header.jsx
+++ React/Personal Project/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react"
 
 function Header() {
   const [isDark, setDark] = useState(false)
@@ -19,7 +19,8 @@ function Header() {
     <section>
       <div className="container-fluid px-lg-5">
         <header className="row d-flex justify-content-between my-2 px-5">
-          <h4 className="col-sm-12 col-md-3 text-center d-flex align-items-center py-2">
+          <h4 className="col-md-12 col-lg-3 text-center d-flex align-items-center py-2">
+
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="30"
@@ -33,18 +34,18 @@ function Header() {
             Quirky Quotes
           </h4>
 
-          <div className="col-sm-12 col-md-4 d-flex justify-content-md-end gap-2">
-            <a href="#features">
-              <button className="btn btn-dark btn-lg border-white">Features</button>
+          <div className="col-md-12 col-lg-6 d-flex justify-content-lg-end row">
+            <a href="#features" className="flex-grow-1 col-lg-3 mb-md-2">
+              <button className="btn btn-dark btn-lg border-white w-100">Features</button>
             </a>
-            <a href="#post">
-              <button className="btn btn-dark btn-lg text-nowrap border-white">Add Quote</button>
+            <a href="#post" className="flex-grow-1 col-lg-3 mb-md-2">
+              <button className="btn btn-dark btn-lg text-nowrap border-white w-100">Add Quote</button>
             </a>
-            <a href="#visitors">
-              <button className="btn btn-dark btn-lg border-white">Visitors</button>
+            <a href="#visitors" className="flex-grow-1 col-lg-3 mb-md-2">
+              <button className="btn btn-dark btn-lg border-white w-100">Visitors</button>
             </a>
-            <a href="#">
-              <button onClick={toDark} className="btn btn-dark btn-lg text-nowrap border-white">
+            <a href="#" className="flex-grow-1 col-lg-3 mb-md-2">
+              <button onClick={toDark} className="btn btn-dark btn-lg text-nowrap border-white w-100">
                 {isDark ? "Light Mode" : "Dark Mode"}
               </button>
             </a>
