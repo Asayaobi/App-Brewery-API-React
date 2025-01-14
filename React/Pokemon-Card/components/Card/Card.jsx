60102cf52120d75f22708274ee26af390da1812a
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-01-14 13:40:28 +0100

    update styles.css for Card

--- React/Pokemon-Card/components/Card/Card.jsx
+++ React/Pokemon-Card/components/Card/Card.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Card({ pokemon }) {
   const { name, url, description } = pokemon;
   return (
-    <div>
-      <p>{name}</p>
-      <p>{url}</p>
-      <p>{description}</p>
+    <div className="card">
+      <img src={url} className="pokemon-image" />
+      <p className="pokemon-name">{name}</p>
+      <p className="pokemon-description">{description}</p>
     </div>
   );
 }
