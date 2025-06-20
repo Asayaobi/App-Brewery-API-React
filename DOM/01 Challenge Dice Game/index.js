dc6096cdc2908af94ac2e49cde3167cf7caa4d0f
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-06-20 22:58:01 +0200

    add winner display

--- DOM/01 Challenge Dice Game/index.js
+++ DOM/01 Challenge Dice Game/index.js
@@ -4,5 +4,15 @@ const diceNumber = () => Math.ceil(Math.random() * 6)
 const user1 = diceNumber()
 const user2 = diceNumber()
 
+//add dice images
 document.querySelector('.img1').setAttribute('src', 'images/dice'+ user1 +'.png')
 document.querySelector('.img2').setAttribute('src', 'images/dice'+ user2 +'.png')
+
+//winner display
+if (user1 === user2){
+    document.querySelector('h1').innerHTML = 'Draw!'
+} else if ( user1 > user2){
+    document.querySelector('h1').innerHTML = 'Player 1 wins!'
+} else {
+    document.querySelector('h1').innerHTML = 'Player 2 wins!'
+}
