d938446cd3d22c08d2373cf67c5eb76cf6384f50
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-06-20 22:37:05 +0200

    add challenge2

--- DOM/00 Challenge/index.js
+++ DOM/00 Challenge/index.js
@@ -1,2 +1,6 @@
 document.querySelector('ul').lastElementChild.innerHTML = 'Tres'
 document.querySelector('.list a').style.color = 'green'
+document.querySelector('button').style.backgroundColor = 'yellow'
+
+document.querySelector('h1').classList.add('huge')
+document.querySelector('a').setAttribute('href', 'https://www.bing.com')
