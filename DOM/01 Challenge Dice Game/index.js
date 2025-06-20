ca840a8a3a029ad0eeefc556d662083434d82ea6
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-06-20 22:53:54 +0200

    add images to users

--- DOM/01 Challenge Dice Game/index.js
+++ DOM/01 Challenge Dice Game/index.js
@@ -1,2 +1,8 @@
 //create random number
 const diceNumber = () => Math.ceil(Math.random() * 6)
+
+const user1 = diceNumber()
+const user2 = diceNumber()
+
+document.querySelector('.img1').setAttribute('src', 'images/dice'+ user1 +'.png')
+document.querySelector('.img2').setAttribute('src', 'images/dice'+ user2 +'.png')
