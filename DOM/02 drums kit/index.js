6b3e806b1f506b7c67979df23040f535b42deef5
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-06-23 22:56:44 +0200

    add switch case expression

--- DOM/02 drums kit/index.js
+++ DOM/02 drums kit/index.js
@@ -25,7 +25,51 @@ const handleClick = () => alert('I got clicked!')
 // }))
 
 //modify css color with addEventListener
+// document.querySelectorAll('.drum').forEach(button => button.addEventListener('click', function(){
+//     console.log(this)
+//     this.style.color = 'white'
+// }))
+
+//using switch case
 document.querySelectorAll('.drum').forEach(button => button.addEventListener('click', function(){
-    console.log(this)
-    this.style.color = 'white'
+    let buttonHTML = this.innerHTML
+    switch(buttonHTML){
+        case'w':
+        const crash = new Audio('sounds/crash.mp3')
+        crash.play()
+        break
+
+        case'a':
+        const kick = new Audio('sounds/kick-bass.mp3')
+        kick.play()
+        break
+
+        case's':
+        const snare = new Audio('sounds/snare.mp3')
+        snare.play()
+        break
+
+        case'd':
+        const tom1 = new Audio('sounds/tom-1.mp3')
+        tom1.play()
+        break
+
+        case'j':
+        const tom2 = new Audio('sounds/tom-2.mp3')
+        tom2.play()
+        break
+
+        case'k':
+        const tom3 = new Audio('sounds/tom-3.mp3')
+        tom3.play()
+        break
+
+        case'l':
+        const tom4 = new Audio('sounds/tom-4.mp3')
+        tom4.play()
+        break
+
+        default:
+        console.log(buttonHTML) //else log this keyword
+    }
 }))
