755afd3916866048db118ae9692a4736b0149f7a
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-06-23 20:36:46 +0200

    create addEventListener

---
+++ DOM/02 drums kit/index.js
@@ -0,0 +1,11 @@
+//addEventListener
+const handleClick = () => alert('I got clicked!')
+// document.querySelectorAll('.drum').forEach(b => b.addEventListener('click', handleClick))
+
+// for (let i = 0; i < document.querySelectorAll('.drum').length; i++){
+//     document.querySelectorAll('button')[i].addEventListener('click', function () {
+//         alert('clicked!')
+//     })
+// }
+
+
