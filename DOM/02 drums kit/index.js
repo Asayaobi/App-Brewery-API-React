9ca8f0f9a00093341f6650be3146502843c8a007
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-06-23 20:52:33 +0200

    modify this property

--- DOM/02 drums kit/index.js
+++ DOM/02 drums kit/index.js
@@ -13,6 +13,19 @@ const handleClick = () => alert('I got clicked!')
 //     const audio = new Audio('sounds/tom-1.mp3')
 //     audio.play()
 // })
-document.querySelectorAll('.drum').forEach(b => b.addEventListener('click', function () {
-    const audio = new Audio('sounds/tom-1.mp3')
-    audio.play()}))
+
+// document.querySelectorAll('.drum').forEach(b => b.addEventListener('click', function () {
+//     const audio = new Audio('sounds/tom-1.mp3')
+//     audio.play()}
+// ))
+
+//see which button got click with this keyword
+// document.querySelectorAll('.drum').forEach(button => button.addEventListener('click', function(){
+//     console.log(this)
+// }))
+
+//modify css color with addEventListener
+document.querySelectorAll('.drum').forEach(button => button.addEventListener('click', function(){
+    console.log(this)
+    this.style.color = 'white'
+}))
