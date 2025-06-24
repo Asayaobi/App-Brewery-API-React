fb138d50fb36dc2432c539bd153c54c2f457fe80
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-06-24 12:35:21 +0200

    adding note

--- DOM/02 drums kit/index.js
+++ DOM/02 drums kit/index.js
@@ -1,5 +1,60 @@
+//playSound function
+const playSound = (key) => {
+    switch(key){
+        case'w':
+        const crash = new Audio('sounds/crash.mp3')
+        crash.play()
+        break
+
+        case'a':
+        const kick = new Audio('sounds/kick-bass.mp3')
+        kick.play()
+        break
+
+        case's':
+        const snare = new Audio('sounds/snare.mp3')
+        snare.play()
+        break
+
+        case'd':
+        const tom1 = new Audio('sounds/tom-1.mp3')
+        tom1.play()
+        break
+
+        case'j':
+        const tom2 = new Audio('sounds/tom-2.mp3')
+        tom2.play()
+        break
+
+        case'k':
+        const tom3 = new Audio('sounds/tom-3.mp3')
+        tom3.play()
+        break
+
+        case'l':
+        const tom4 = new Audio('sounds/tom-4.mp3')
+        tom4.play()
+        break
+
+        default:
+        console.log(buttonHTML) //else log this keyword
+    }
+}
+
+//with click
+document.querySelectorAll('.drum').forEach(button => button.addEventListener('click', function(event){
+    playSound(event.target.innerHTML)
+}))
+//with keydown
+document.addEventListener('keydown', function(event) {
+    playSound(event.key)
+})
+
+
+
+//note
 //addEventListener
-const handleClick = () => alert('I got clicked!')
+// const handleClick = () => alert('I got clicked!')
 // document.querySelectorAll('.drum').forEach(b => b.addEventListener('click', handleClick))
 
 // for (let i = 0; i < document.querySelectorAll('.drum').length; i++){
@@ -78,55 +133,3 @@ const handleClick = () => alert('I got clicked!')
 // document.addEventListener('keydown', function(event){
 //     console.log(event.key) //KeyboardEvent {isTrusted: true, key: 'g', code: 'KeyG', location: 0, ctrlKey: false, …}
 // })
-
-//playSound function
-const playSound = (key) => {
-    switch(key){
-        case'w':
-        const crash = new Audio('sounds/crash.mp3')
-        crash.play()
-        break
-
-        case'a':
-        const kick = new Audio('sounds/kick-bass.mp3')
-        kick.play()
-        break
-
-        case's':
-        const snare = new Audio('sounds/snare.mp3')
-        snare.play()
-        break
-
-        case'd':
-        const tom1 = new Audio('sounds/tom-1.mp3')
-        tom1.play()
-        break
-
-        case'j':
-        const tom2 = new Audio('sounds/tom-2.mp3')
-        tom2.play()
-        break
-
-        case'k':
-        const tom3 = new Audio('sounds/tom-3.mp3')
-        tom3.play()
-        break
-
-        case'l':
-        const tom4 = new Audio('sounds/tom-4.mp3')
-        tom4.play()
-        break
-
-        default:
-        console.log(buttonHTML) //else log this keyword
-    }
-}
-
-//with click
-document.querySelectorAll('.drum').forEach(button => button.addEventListener('click', function(event){
-    playSound(event.target.innerHTML)
-}))
-//with keydown
-document.addEventListener('keydown', function(event) {
-    playSound(event.key)
-})
