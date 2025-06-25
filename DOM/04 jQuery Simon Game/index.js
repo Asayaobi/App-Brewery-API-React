7ca75d1563e0f801efc82262b476691c1822c9ce
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-06-25 22:09:57 +0200

    add animation to color

--- DOM/04 jQuery Simon Game/index.js
+++ DOM/04 jQuery Simon Game/index.js
@@ -3,8 +3,14 @@ const gamePattern = []
 
 const nextSequence = () => {
     //get new color
-    const randomNum = Math.ceil(Math.random() * 4)
+    const randomNum = Math.floor(Math.random() * 4)
     const randomColor = buttonColors[randomNum]
     gamePattern.push(randomColor)
-
+    //add animation to button
+    $(`#${randomColor}`).fadeOut(100).fadeIn(100)
+    //add sound
+    const sound = new Audio(`sounds/${randomColor}.mp3`)
+    sound.play()
 }
+
+nextSequence()
