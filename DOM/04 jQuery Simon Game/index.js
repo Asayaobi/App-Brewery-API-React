630adef3b012c7d20aaa16f4f399bb1b918de2d7
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-06-27 14:42:20 +0200

    add styling when user selects wrong answer

--- DOM/04 jQuery Simon Game/index.js
+++ DOM/04 jQuery Simon Game/index.js
@@ -59,5 +59,11 @@ const checkAnswer = (currentLevel) => {
     }
     } else {
         console.log('wrong')
+        playSound('wrong')
+        $('body').addClass('game-over')
+        $('#level-title').text('Game Over, Press Any Key to Restart')
+        setTimeout(function(){
+            $('body').removeClass('game-over')
+        },2000)
     }
 }
