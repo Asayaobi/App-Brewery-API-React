6f6e9f128853fcb6d121138e0a05a213b11cfeb5
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-06-28 20:35:20 +0200

    adding /contact and  /about

--- Backend/2.5 Endpoint/index.js
+++ Backend/2.5 Endpoint/index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
     res.send('<h1>Welcome to my site</h1>')
 });
 
+app.get('/contact', (req, res) => {
+    res.send('<h1>Contact me</h1>')
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About me</h1>')
+});
+
 app.listen(port, () => {
     console.log(`server running on port: ${port}`)
 });
