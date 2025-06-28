e155b3449afd0fbcba71c4ecb39ccf5b5706554e
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-06-28 22:35:01 +0200

    mount body parser middleware

--- Backend/3.4 Middleware/index1.js
+++ Backend/3.4 Middleware/index1.js
@@ -1,14 +1,27 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+//body parser middleware
+import bodyParser from 'body-parser'
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+//mount body parser middleware using express
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
+
+//write post handler to console log the content
+app.post("/submit", (req,res) => {
+  console.log(req.body)//{ text: 'hello' }
+  res.send(req.body)
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
