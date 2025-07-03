5d55d9b7f305eb4df7c4f7b06d08cff415ec46a7
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-07-03 22:54:39 +0200

    add home image

--- React/Personal Project- OpenD/src/App.jsx
+++ React/Personal Project- OpenD/src/App.jsx
@@ -1,12 +1,13 @@
-// import { useState } from 'react'
 import './App.css'
 import Header from './Header'
+import homeImage from "/home-img.png"
 
 function App() {
 
   return (
     <>
     <Header />
+    <img className="bottom-space" src={homeImage} />
     </>
   )
 }
