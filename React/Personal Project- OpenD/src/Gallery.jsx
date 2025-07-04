30a1f69c51e99c5c9b8be6fe815dba1f09148cfa
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-07-04 23:10:24 +0200

    Update props on Gallery

--- React/Personal Project- OpenD/src/Gallery.jsx
+++ React/Personal Project- OpenD/src/Gallery.jsx
@@ -1,13 +1,14 @@
 import Item from "./Item"
 
-function Gallery() {
+function Gallery(props) {
+  // console.log(`gallery props ${props}`)
   return (
     <div className="gallery-view">
       <h3 className="makeStyles-title-99 Typography-h3">Discover</h3>
       <div className="disGrid-root disGrid-container disGrid-spacing-xs-2">
         <div className="disGrid-root disGrid-item disGrid-grid-xs-12">
           <div className="disGrid-root disGrid-container disGrid-spacing-xs-5 disGrid-justify-content-xs-center"></div>
-          <Item />
+          {props.data && props.data.map((d,index) => <Item key={index} data={d}/>)}
         </div>
       </div>
     </div>
