369479b6510a3c062e38464b64777630be0274aa
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-07-04 15:55:34 +0200

    update passing props in App component

--- React/Personal Project- OpenD/src/App.jsx
+++ React/Personal Project- OpenD/src/App.jsx
@@ -2,16 +2,18 @@ import './App.css'
 import Header from './Header'
 import homeImage from "/home-img.png"
 import Footer from './Footer'
-import Item from './Item'
-import nfts from '../data'
+import Minter from './Minter'
+// import Item from './Item'
+// import nfts from '../data'
 
 function App() {
   return (
     <>
     <Header />
-{nfts.map((nft, index) => <Item  key={index} owner={nft.owner} id={nft.id} artist={nft.artist} img=
+    <Minter />
+{/* {nfts.map((nft, index) => <Item  key={index} owner={nft.owner} title={nft.artist} img=
 {nft.img}/>)
-}    
+}     */}
     <img className="bottom-space" src={homeImage} />
     <Footer />
     </>
