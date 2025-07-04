825ddfbdbb55e8488fc37cfe9613a911768c0751
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-07-04 14:05:57 +0200

    render data.ja to App

--- React/Personal Project- OpenD/src/App.jsx
+++ React/Personal Project- OpenD/src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import Header from './Header'
 import homeImage from "/home-img.png"
 import Footer from './Footer'
+import Item from './Item'
+import nfts from '../data'
 
 function App() {
-
   return (
     <>
     <Header />
+{nfts.map((nft, index) => <Item  key={index} owner={nft.owner} id={nft.id} artist={nft.artist} img=
+{nft.img}/>)
+}    
     <img className="bottom-space" src={homeImage} />
     <Footer />
     </>
