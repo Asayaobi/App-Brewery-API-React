aa78a51894817f5727ae5b0b4360c8e3f0e617e0
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-07-04 14:05:47 +0200

    Create data.js

---
+++ React/Personal Project- OpenD/data.js
@@ -0,0 +1,27 @@
+  const nfts = [{
+      id: 132,
+      artist: "Cryptopunk",
+      img: "https://www.parsehub.com/blog/content/images/2022/01/cp-6275.png",
+      owner: "NFT Gal"
+  },
+  {
+      id: 133,
+      artist: "Cryptopunk",
+      img: "https://www.parsehub.com/blog/content/images/2022/01/cp-2338.png",
+      owner: "NFT Gal"
+  },
+    {
+      id: 434,
+      artist: "Cryptopunk",
+      img: "https://www.parsehub.com/blog/content/images/2022/01/cp-2140.png",
+      owner: "Artic Monkey"
+  },
+      {
+      id: 144,
+      artist: "Cryptopunk",
+      img: "https://www.parsehub.com/blog/content/images/2022/01/cp---8857.png",
+      owner: "See Me 007"
+  },
+]
+
+export default nfts
