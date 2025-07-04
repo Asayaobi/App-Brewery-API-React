b4e551b37183d273d067c7d65381a2444c6ec9c8
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-07-04 14:06:08 +0200

    render data on Item

--- React/Personal Project- OpenD/src/Item.jsx
+++ React/Personal Project- OpenD/src/Item.jsx
@@ -1,24 +1,32 @@
-import logo from "/logo.png"
+import PropTypes from 'prop-types'
 
-function Item() {
+function Item(props) {
+
   return (
     <div className="disGrid-item">
       <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
         <img
           className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
-          src={logo}
+          src={props.img}
         />
         <div className="disCardContent-root">
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-            CryptoDunks #312<span className="purple-text"></span>
+            {props.artist} #{props.id}<span className="purple-text"></span>
           </h2>
           <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
-            Owner: sdfsdf-erwerv-sdf
+            Owner: {props.owner}
           </p>
         </div>
       </div>
     </div>
   )
+}
+
+Item.propTypes = {
+  img: PropTypes.string.isRequired,
+  artist: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  owner: PropTypes.string.isRequired,
 }
 
 export default Item
