ba0d2b54eb431cdf767e93fd4997c9e313b5d2c4
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-07-04 19:54:49 +0200

    update local data function in App

--- React/Personal Project- OpenD/src/App.jsx
+++ React/Personal Project- OpenD/src/App.jsx
@@ -3,17 +3,25 @@ import Header from './Header'
 import homeImage from "/home-img.png"
 import Footer from './Footer'
 import Minter from './Minter'
-// import Item from './Item'
-// import nfts from '../data'
+import Item from './Item'
+import nfts from '../data'
+import { useState } from 'react'
 
 function App() {
+  const [data, setData] = useState(nfts)
+
+  function updateData(newNft){
+    console.log(newNft)
+    setData([newNft, ...data])
+    console.log(data)
+  }
   return (
     <>
     <Header />
-    <Minter />
-{/* {nfts.map((nft, index) => <Item  key={index} owner={nft.owner} title={nft.artist} img=
-{nft.img}/>)
-}     */}
+    <Minter addNft={updateData}/>
+{data.map((nft, index) => <Item  key={index} owner={nft.owner} title={nft.title} img=
+{nft.img} />)
+}    
     <img className="bottom-space" src={homeImage} />
     <Footer />
     </>
