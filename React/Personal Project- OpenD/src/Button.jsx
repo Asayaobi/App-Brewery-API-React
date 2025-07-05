53020b0d1b7751c49f7065bbd417d735badd6e71
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-07-05 21:42:12 +0200

    Create Button.jsx

---
+++ React/Personal Project- OpenD/src/Button.jsx
@@ -0,0 +1,13 @@
+function Button(props){
+    return(
+        <div className="Chip-root makeStyles-chipBlue-108 Chip-clickable">
+            <span
+              onClick={props.handleClick}
+              className="form-Chip-label"
+            >
+              Sell
+            </span>
+            </div>
+    )
+}
+export default Button
