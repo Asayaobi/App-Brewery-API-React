d6d20b07c73c233c72e1eeab3de974ecc24ceb0a
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-07-08 15:20:03 +0200

    Update data.js

--- React/Personal Project- OpenD/data.js
+++ React/Personal Project- OpenD/data.js
@@ -1,25 +1,25 @@
   const nfts = [{
       title: "Cryptopunk #112",
       img: "https://www.parsehub.com/blog/content/images/2022/01/cp-6275.png",
-      owner: "NFT Gal",
+      owner: "OpenD",
       price: 2
   },
   {
       title: "Cryptopunk #32",
       img: "https://www.parsehub.com/blog/content/images/2022/01/cp-2338.png",
-      owner: "NFT Gal",
+      owner: "OpenD",
       price: 3
   },
     {
       title: "Cryptopunk #43",
       img: "https://www.parsehub.com/blog/content/images/2022/01/cp-2140.png",
-      owner: "Artic Monkey",
+      owner: "OpenD",
       price: 12
   },
       {
       title: "Cryptopunk #56",
       img: "https://www.parsehub.com/blog/content/images/2022/01/cp---8857.png",
-      owner: "See Me 007",
+      owner: "OpenD",
       price: 6
   },
 ]
